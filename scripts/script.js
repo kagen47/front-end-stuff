0e50c0d142644913fee3574a3650e12b33e1067e
Gene Nakagaki <gene.nakagaki@gmail.com>
2019-01-23 22:36:35 -0500

    some commit

--- scripts/script.js
+++ scripts/script.js
@@ -22,39 +22,68 @@ function initExpandable() {
 }
 
 function initFixedCenterable() {
-    document.querySelectorAll('.fes.fixed-centerable').forEach(function (elem) {
-        elem.addEventListener('click', function() {
+    document.querySelectorAll('.fes.fixed-centerable-container').forEach(function (elem) {
+        elem.style.width = elem.offsetWidth + 'px';
+        elem.style.height = elem.offsetHeight + 'px';
+
+        var fixedCenterable = elem.querySelector('.fixed-centerable');
+        fixedCenterable.fesOrigOffsetTop = fixedCenterable.offsetTop;
+        fixedCenterable.fesOrigLeft = fixedCenterable.offsetLeft + 'px';
+        fixedCenterable.addEventListener('click', function () {
+            console.log("fixed centerable clicked");
+            console.log(this);
+
             if (!this.classList.contains('active')) {
-                var top = this.offsetTop - window.pageYOffset + 'px';
-                var left = this.offsetLeft + 'px';
+                console.log("not active");
                 this.style.position = 'fixed';
-                this.style.top = top;
-                this.style.left = left;
-                console.log(top);
+                this.style.top = this.fesOrigOffsetTop - window.pageYOffset + 'px';
+                this.style.left = this.fesOrigLeft;
+
                 this.classList.add('active');
-                
-                this.style.left = '50%';
+                var that = this;
+                setTimeout(function () {
+                    that.style.top = '50%';
+                    that.style.left = '50%';
+                    that.style.transform = 'translate(-50%, -50%)';
+                    that.style.webkitTransform = 'translate(-50%, -50%)';
+                }, 50);
             } else {
-                this.classList.remove('active');
+                console.log('is active');
+                this.style.top = this.fesOrigOffsetTop - window.pageYOffset + 'px';
+                this.style.left = this.fesOrigLeft;
+                this.style.transform = 'none';
+                this.style.webkitTransform = 'none';
+                var that = this;
+                setTimeout(function () {
+                    that.style.position = 'relative';
+                    that.style.top = '0';
+                    that.style.left = '0';
+                    that.classList.remove('active');
+                }, 300);
             }
         });
     });
+
 }
 
 function initBackgroundOverlay() {
     document.querySelectorAll('.fes.background-overlay').forEach(function (elem) {
         var trigger = document.querySelector(elem.dataset.trigger);
         trigger.backgroundOverlay = elem;
+
         console.log(trigger);
 
         trigger.addEventListener('click', function () {
-            console.log('here');
+            console.log("background overlay trigger clicked")
+
             if (!this.backgroundOverlay.classList.contains('active')) {
                 this.backgroundOverlay.classList.add('active');
             }
         });
 
         elem.addEventListener('click', function () {
+            console.log("background overlay clicked");
+
             if (this.classList.contains('active')) {
                 this.classList.remove('active');
             }
